e8d0a03505752d964878b080d8feabdfb684ebc7
GoofyGoober155 <dewabrata155@gmail.com>
2024-11-12 18:34:22 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/acnescan/ui/Register.kt
@@ -0,0 +1,12 @@
+package com.dicoding.acnescan.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.acnescan.R
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
